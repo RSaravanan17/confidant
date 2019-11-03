e2c78883dbeeb056ed8e3c40490be6ba966b867e
Amit Joshi <amit.joshiusa@gmail.com>
2019-11-02 23:57:46 -0500

    sentsim with nextquestion

--- flask/sentencesimilarity.py
+++ flask/sentencesimilarity.py
@@ -3,6 +3,12 @@ import string
 import json
 url = "https://twinword-text-similarity-v1.p.rapidapi.com/similarity/"
 
+idToQuestion = dict()
+idToQuestion[0] = "My dog died."
+
+questionToId = dict()
+questionToId["My dog died."] = 0
+
 questionToAnswer = dict()
 questionToAnswer["My dog died."] = ["I'm really sorry to hear that, it will get better soon.", "My deepest condolences, I empathize with you."]
 
@@ -10,11 +16,18 @@ headers = {
     'x-rapidapi-host': "twinword-text-similarity-v1.p.rapidapi.com",
     'x-rapidapi-key': "66707436f8mshde697637b6499e2p106547jsn2e624f4dd810"
     }
+
 def private(text1, text2):
 	querystring = {"text1": text1,"text2": text2}
 	response = requests.request("GET", url, headers=headers, params=querystring)
 	obj = json.loads(response.text)
 	return obj['similarity']
+
+def getNextQuestion(curQuestion):
+	if curQuestion == "0" or curQuestion == "" or curQuestion == None or curQuestion == 0:
+		return idToQuestion[0]
+	else:
+		return idToQuestion[questionToId[question] + 1]
 
 def computeSentenceSimilarity(question, usersaid):
 	answers = questionToAnswer[question]
