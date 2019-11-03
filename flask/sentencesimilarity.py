af9c75aae46a66290781e5d50dd937764996268f
Amit Joshi <amit.joshiusa@gmail.com>
2019-11-03 10:39:47 -0600

    more questions

--- flask/sentencesimilarity.py
+++ flask/sentencesimilarity.py
@@ -1,18 +1,23 @@
 import requests
 import string
 import json
+import math
 
 import random
 url = "https://twinword-text-similarity-v1.p.rapidapi.com/similarity/"
 
 currentQuestionId = 0
-idToQuestion = ["My dog died. ", "I really miss my dog. His name was Buddy.", "I hope Buddy is happy, wherever he is."]
+idToQuestion = ["My dog died. ", "I really miss my dog. His name was Buddy.", "I hope Buddy is happy, wherever he is.", "I got promoted at work!", "I am getting married!", "My girlfriend broke up with me."]
 goodResponses = ["Thanks for understanding, ", "I'm grateful that you get it, ", "You're a good listener, "]
 badResponses = ["You're so rude, ", "Why would you say that? " , "Don't be so mean, "]
 
-questionToAnswer = [[], [], []]
-questionToAnswer[0] = ["I'm really sorry to hear that, it will get better soon.", "My deepest condolences, I empathize with you."]
-questionToAnswer[1] = ["It's okay to miss loved ones. ", "I miss him too", "I'm sure Buddy misses you too."]
+questionToAnswer = list()
+questionToAnswer.append(["I'm really sorry to hear that, it will get better soon.", "My deepest condolences, I empathize with you."])
+questionToAnswer.append(["It's okay to miss loved ones. ", "I miss him too", "I'm sure Buddy misses you too."])
+questionToAnswer.append(["May he rest in peace in heaven.", "I am sure he is happy too.", "He is happy in paradise."])
+questionToAnswer.append(["That's great to hear!", "Super cool! Keep it up!", "You did a great job!"])
+questionToAnswer.append(["Wow you are so lucky!", "Wishing you a happy and loving life!", "Congratulations on the engagement!"])
+questionToAnswer.append(["I'm so sorry.", "You will get through this, don't worry.", "You are better off without her, you are an amazing."])
 
 headers = {
     'x-rapidapi-host': "twinword-text-similarity-v1.p.rapidapi.com",
@@ -27,18 +32,23 @@ def private(text1, text2):
 
 def getNextResponse(score):
 	emoResponse = ""
-	if score > 0.35: 
+	if score > 0.25: 
 		emoResponse = goodResponses[random.randint(0, len(goodResponses)-1)]
 	else:
 		emoResponse = badResponses[random.randint(0, len(badResponses)-1)]
 	global currentQuestionId
-	currentQuestionId = currentQuestionId + 1
+	currentQuestionId = (currentQuestionId + 1) % len(questionToAnswer)
 	return emoResponse + getQuestion(currentQuestionId)
+
 def getFirstQuestion():
-	currentQuestionId = 0
-	return idToQuestion[0]
+	global startIndex
+	currentQuestionId = random.randint(0, len(questionToAnswer)-1)
+	startIndex = currentQuestionId
+	return idToQuestion[currentQuestionId] #dont call getQuestion here, keep it as idToQuestion[currentQuestionId]
+
 def getQuestion(id):
-	if(id > len(idToQuestion)):
+	global startIndex
+	if(id == startIndex):
 		return ""
 	currentQuestion = idToQuestion[id]
 	return currentQuestion
