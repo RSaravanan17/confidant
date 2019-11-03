b2fc8f0066ddc6375724828537e23904d2ecb2ce
rithvik <rithvik.saravanan17@gmail.com>
2019-11-02 20:59:45 -0500

    Added gaze score

--- public/headViewScript.js
+++ public/headViewScript.js
@@ -1,12 +1,23 @@
 
 function waitForWebgazer(){
     if(typeof webgazer !== "undefined"){
+        var good = 0;  // number of gazes within bounding box
+        var total = 0;  // number of gazes total
+
         webgazer.setGazeListener(function(data, elapsedTime){
             if (data == null) {
                 return;
             }
-            var xprediction = data.x; //these x coordinates are relative to the viewport
-            var yprediction = data.y; //these y coordinates are relative to the viewport
+
+            var xprediction = data.x;  // these x coordinates are relative to the viewport
+            var yprediction = data.y;  // these y coordinates are relative to the viewport
+            
+            total++;
+
+            // bounding box for gaze
+            if (xprediction > 350 && xprediction < 600 && yprediction > 25 && yprediction < 625) good++;
+
+            console.log("score at " + elapsedTime + ": " + good + " ~ " + total);
         }).begin();
     }
     else{
